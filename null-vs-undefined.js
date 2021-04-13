97b62964c34b87e7306b472264b024021ee30d99
smartlycoding <joinalzx@gmail.com>
2021-04-13 07:24:12 +0600

    null vs undefined for interview question

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// let pakhi;
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add (num1, num2=11){
+//     console.log(num1,num2)
+// }
+// const premik = {name: "smart dude", phone: 234234523}
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log (fun);
+
+null
+//undefined onek babe pete paro, 
+let ages = [3, 5, 9];
+console.log(ages[11]);
