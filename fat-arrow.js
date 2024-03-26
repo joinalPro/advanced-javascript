6fbc4fedf90e338f3e0efc5085ebccdd41e96ba2
Joinal Abden <joinalzx@gmail.com>
2024-03-26 15:17:25 +0600

    fat arrow

---
+++ fat-arrow.js
@@ -0,0 +1,21 @@
+// Es6 fat arrow function
+// function number (){
+//     return 10;
+// }
+// function number (n){
+//     return 10;
+// }
+
+// let number = () => 10;
+// let number = () => console.log(10);
+// let number = () => 10;
+  
+// number()
+
+var javaScript = {
+    name: "JavaScript",
+    libraries: ["React", "Angular", "Vue"],
+    printLibraries: function (){
+        this.libraries.forEach()
+    }
+}
